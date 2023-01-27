4b868c07332d524010612932f8ee56dc36c18ab6
Yuval <yuvaltimen@gmail.com>
2023-01-27 00:29:49 -0500

    Minimum viable product

--- ascii_art.py
+++ ascii_art.py
@@ -22,7 +22,7 @@ def main(path: str = None) -> None:
 def do_downsample(img: np.array) -> np.array:
     
     
-    downsampled_img = block_reduce(img, block_size=(19, 19, 1), func=np.max)
+    downsampled_img = block_reduce(img, block_size=(13, 5, 1), func=np.max)
     print(img.shape)
     print(downsampled_img.shape)
     
@@ -30,22 +30,33 @@ def do_downsample(img: np.array) -> np.array:
     # plt.show()
     
     return downsampled_img
+
+def dist(p: tuple, q: tuple) -> float:
+    return math.sqrt(sum([math.pow(p[i] - q[i], 2) for i in range(len(p))]))
     
 def image_to_ascii(img: np.array) -> str:
     
-    EMPTY = "."
-    LIGHT = "+"
-    DARK = "#"
+    cmap = """$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/()1{}[]?-_+~<>i!lI;:,"^`'."""
     
+    out = ""
     
-    
-    for x in range(img.shape[0]):
-        for y in range(img.shape[1]):
-    
-            color = img[x][y]
-            print(color)
+    max_dist = dist((255, 255, 255), (0, 0, 0))
+    print("max:" + str(max_dist))
+
+    for w in range(img.shape[0]):
         
-    return "hit ascii"
+        for h in range(img.shape[1]):
+            color = img[w][h]
+            print("color:" + str(color))
+            distance = dist(color, (0, 0, 0))
+            print("dist:" + str(distance))
+            thresh = int(np.interp((distance - 1) / max_dist, [0, 1], [0, len(cmap)]))
+            print("thresh:" + str(thresh))
+            out += cmap[thresh]
+                
+        out += "\n"
+        
+    return out
 
 
 
